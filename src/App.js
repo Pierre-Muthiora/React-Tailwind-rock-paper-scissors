6bb9e9926cbca2dfd842b99b7d434aa7e873c4ea
Peter Ng'ang'a <pierremuthiora@gmail.com>
2023-04-11 13:51:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+
 
 function App() {
+  const [myChoice, setMyChoice] = useState('rock')
+  const [computerChoice, setComputerChoice] = useState('rock')
+  const [myPoints, setMyPoints] = useState(0)
+  const [computerPoints, setComputerPoints] = useState(0)
+  const [scoreMessage, setScoreMessage] = useState(null)
+  const [finalResult, setFinalResult] = useState('')
+  const [gameOver, setGameOver] = useState(false)
+
+  const choice = ['rock', 'paper', 'scissors']
+
+  const handleOnClick = (choice) => {
+    setMyChoice(choice)
+    generateComputerChoice()
+  }
+  const generateComputerChoice = () => {
+    const randomChoice = choice[Math.floor(Math.random() * 3)]
+    setComputerChoice(randomChoice)
+  }
+  const restart = () => {
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    const combinedMoves = myChoice + computerChoice
+    if (myPoints <= 4 && computerPoints <= 4) {
+      if (combinedMoves === 'rockscissors' || combinedMoves === 'paperrock' || combinedMoves === 'scissorspaper') {
+        const updatedUserPoints = myPoints + 1
+        setMyPoints(updatedUserPoints)
+        setScoreMessage('You scored')
+        if (updatedUserPoints === 5) {
+          setGameOver(true)
+          setFinalResult('You win')
+        }
+      }
+      if (combinedMoves === 'paperscissors' || combinedMoves === 'scissorsrock' || combinedMoves === 'rockpaper') {
+        const updatedComputerPoints = computerPoints + 1
+        setComputerPoints(updatedComputerPoints)
+        setScoreMessage('Computer scored')
+        if (updatedComputerPoints === 5) {
+          setGameOver(true)
+          setFinalResult('Computer wins')
+        }
+      }
+      if (combinedMoves === 'rockrock' || combinedMoves === 'paperpaper' || combinedMoves === 'scissorsscissors') {
+        setScoreMessage('Tie')
+      }
+    }
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [myChoice, computerChoice])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center text-center'>
+
+      <section className='w-full'>
+        <div>
+          <h1 className='text-6xl font-extrabold'>
+            Rock Paper Scissors
+          </h1>
+        </div>
+        <div className='flex justify-around items-center h-28 text-4xl font-bold'>
+          <div>
+            <h3>You</h3>
+            <p className='font-normal text-[#fb6837]'>{myPoints}</p>
+          </div>
+          <div>
+            <h3>Computer</h3>
+            <p className='font-normal text-[#fb6837]'>{computerPoints}</p>
+          </div>
+        </div>
+      </section>
+
+      <section className='relative'>
+        <div className={gameOver ? 'opacity-0 transition duration-700 ease-in' : 'opacity-100'}>
+          <div className='flex justify-center space-x-14 h-60'>
+            <div className='w-64'>
+              <img src={`/assets/${myChoice}2.png`} alt="Your hand" />
+            </div>
+            <div className='w-64'>
+              <img src={`/assets/${computerChoice}.png`} alt="Computer hand" />
+            </div>
+          </div>
+
+          <div className='flex justify-center items-center'>
+            {choice.map((choice, index) =>
+              <button className='bg-[#48b8bc] border font-bold rounded-md p-3 w-28 text-xl cursor-pointer' key={index} onClick={() => handleOnClick(choice)} disabled={gameOver}>{choice}</button>
+            )}
+          </div>
+          <div className='p-10'>
+            <h1 className='text-4xl font-bold tracking-widest'>{scoreMessage}</h1>
+          </div>
+        </div>
+
+        <section className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[529px] h-[378px] font-semibold'>
+          <h2 className='text-5xl tracking-widest mt-16'>{finalResult}</h2>
+          {gameOver && <button className='bg-[#fb6837] mt-32 border font-bold rounded-md p-3 w-28 text-xl cursor-pointer' onClick={() => restart()}>Restart</button>}
+        </section>
+
+      </section>
     </div>
+
+
+
   );
 }
 
